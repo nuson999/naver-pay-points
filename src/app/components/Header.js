4a5bc25d8765673fa158740db0bf86c94460551e
nuson999 <eungangku@gmail.com>
2024-03-02 19:32:12 +0900

    Add Chip component to Header.js

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -6,6 +6,7 @@ import Typography from "@mui/joy/Typography";
 import { GitHub } from "@mui/icons-material";
 import ShareIcon from "@mui/icons-material/Share";
 import IconButton from "@mui/joy/IconButton";
+import Chip from "@mui/joy/Chip";
 
 function Header() {
   const copyUrlToClipboard = () => {
@@ -17,8 +18,15 @@ function Header() {
       <Grid container spacing={3}>
         <Grid>
           <Box>
-            <Typography level="h1">네이버 페이 적립 모음</Typography>
-            <Typography level="h5">오늘 00:45 업데이트됨</Typography>
+            <Typography level="h1" textColor="#03c75a" sx={{ display: "inline-block", backgroundColor: "white", paddingX: "10px", borderRadius: "10px", marginRight: "10px" }}>
+              N
+            </Typography>
+            <Typography level="h1" sx={{ display: "inline-block" }}>
+              페이 적립 모음
+            </Typography>
+            <Typography level="h5" sx={{ marginTop: "10px" }}>
+              오늘 00:45 업데이트됨
+            </Typography>
           </Box>
         </Grid>
         <Grid>
